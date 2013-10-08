92b351984ff1c00d1506490993d0ecb2c6e03e31
giginet <giginet.net@gmail.com>
2013-10-09 03:53:09 +0900

    Add iptables module

--- install_modules.sh
+++ install_modules.sh
@@ -2,6 +2,7 @@ if type "puppet" > /dev/null; then
   puppet install module alup/rbenv 
   puppet install module puppetlabs/apache
   puppet install module puppetlabs/mysql
+  puppet module install erwbgy/iptables
 else
   echo "Please install puppet"
 fi
--- manifests/init.pp
+++ manifests/init.pp
@@ -7,6 +7,9 @@ class kawaz_redmine {
   include database
   include redmine
   include server
+  include iptables
+
+  iptables::allow { 'tcp/80': port => '80', protocol => 'tcp' }
 
   Class['package::install'] 
   -> Class['user'] 
