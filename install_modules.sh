d069f278eb72418051f488b2785c870ab556c3bc
giginet <giginet.net@gmail.com>
2013-10-09 03:54:17 +0900

    Fix module install script error.

--- install_modules.sh
+++ install_modules.sh
@@ -1,7 +1,7 @@
 if type "puppet" > /dev/null; then
-  puppet install module alup/rbenv 
-  puppet install module puppetlabs/apache
-  puppet install module puppetlabs/mysql
+  puppet module install alup/rbenv 
+  puppet module install puppetlabs/apache
+  puppet module install puppetlabs/mysql
   puppet module install erwbgy/iptables
 else
   echo "Please install puppet"
